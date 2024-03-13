0e12775cf522c9efce2e078cba1af4b8585386b5
JoshuaY <yu.zhuoc@northeastern.edu>
2024-03-13 14:31:11 -0700

    lab6

---
+++ lab6/capers/CapersRepository.java
@@ -0,0 +1,89 @@
+package capers;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+
+import static capers.Utils.*;
+
+/** A repository for Capers
+ * @author Josh Yu
+ * The structure of a Capers Repository is as follows:
+ *
+ * .capers/ -- top level folder for all persistent data in your lab12 folder
+ *    - dogs/ -- folder containing all of the persistent data for dogs
+ *    - story -- file containing the current story
+ *
+ *
+ */
+public class CapersRepository {
+    /** Current Working Directory. */
+    static final File CWD = new File(System.getProperty("user.dir"));
+
+    /** Main metadata folder. */
+    static final File CAPERS_FOLDER = Utils.join(CWD,".capers");
+
+    /**
+     * Does required filesystem operations to allow for persistence.
+     * (creates any necessary folders or files)
+     * Remember: recommended structure (you do not have to follow):
+     *
+     * .capers/ -- top level folder for all persistent data in your lab12 folder
+     *    - dogs/ -- folder containing all of the persistent data for dogs
+     *    - story -- file containing the current story
+     */
+    public static void setupPersistence() {
+        if(!CAPERS_FOLDER.exists()){
+            CAPERS_FOLDER.mkdir();
+        }
+        if(!Dog.DOG_FOLDER.exists()){
+            Dog.DOG_FOLDER.mkdir();
+        }
+
+        }
+
+    /**
+     * Appends the first non-command argument in args
+     * to a file called `story` in the .capers directory.
+     * @param text String of the text to be appended to the story
+     */
+    public static void writeStory(String text) {
+        File STORY = Utils.join(CAPERS_FOLDER.getPath(),"story");
+        String updatedContent;
+        if(!STORY.exists()) {
+            updatedContent = text;
+        }else{
+            String existingContent = Utils.readContentsAsString(STORY);
+
+            // Append the new text to the existing content
+            updatedContent = existingContent + "\n" + text;
+        }// Write the updated content back to the file
+        Utils.writeContents(STORY, updatedContent);
+        System.out.println(updatedContent);
+    }
+
+
+    /**
+     * Creates and persistently saves a dog using the first
+     * three non-command arguments of args (name, breed, age).
+     * Also prints out the dog's information using toString().
+     */
+    public static void makeDog(String name, String breed, int age) {
+        Dog dog = new Dog(name, breed, age);
+        dog.saveDog();
+        System.out.println(dog);
+    }
+
+    /**
+     * Advances a dog's age persistently and prints out a celebratory message.
+     * Also prints out the dog's information using toString().
+     * Chooses dog to advance based on the first non-command argument of args.
+     * @param name String name of the Dog whose birthday we're celebrating.
+     */
+    public static void celebrateBirthday(String name) {
+
+        Dog dog = Dog.fromFile(name);
+        dog.haveBirthday();
+        dog.saveDog();
+    }
+}
